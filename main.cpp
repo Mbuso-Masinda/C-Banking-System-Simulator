b57503e289a99ceffeac78b73284e906a32bde0f
mbuso-masinda <21323283@mylife.unisa.ac.za>
2026-03-12 18:11:56 +0200

    clearing main

--- main.cpp
+++ main.cpp
@@ -2,20 +2,6 @@
 #include "include/Bank.h"
 
 int main() {
-    Bank bank;
 
-    bank.loadBank();
-
-    bank.updateBalance(bank.getUsers()[1].getAccounts()[0]);
-    std::cout << bank.getUsers()[1].getAccounts()[0].getBalance();
-
-    Transaction t("TR323", "12334", "BANK", "12345", "DEPOSIT", 600);
-
-    bank.deposit(t);
-
-    std::cout << std::endl;
-    bank.updateBalance(bank.getUsers()[1].getAccounts()[0]);
-    std::cout << bank.getUsers()[0].getAccounts()[0].getBalance() << std::endl;
-    std::cout << bank.getUsers()[1].getAccounts()[0].getBalance();
-
+    return 0;
 }
